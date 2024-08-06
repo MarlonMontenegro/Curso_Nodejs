c84d18cb1da6b2cbaf554d45faaacc26d5c3e4be
Marlon Montenegro <marlonmontenegropaz@gmail.com>
2024-08-05 19:06:52 -0600

    Second Class

---
+++ 9.http.js
@@ -0,0 +1,13 @@
+const http = require("node:http");
+
+const server = http.createServer((req, res) => {
+  console.log("request received");
+
+  res.end("Hola Mundo");
+});
+
+server.listen(0, () => {
+  console.log(
+    `server listening on port http://localhost:${server.address().port}`
+  );
+});
